90a246c4ddf1ad2225bccdb8effdd8a46b32f0b5
Sleeper678 <umardika678@gmail.com>
2024-09-16 17:22:00 +0700

    Navbar

---
+++ script.js
@@ -0,0 +1,21 @@
+const searchBar = document.querySelector(".search-input");
+const searchButton = document.getElementById("search-button");
+
+searchButton.addEventListener("dblclick", function () {
+  searchBar.classList.add("-translate-y-full");
+  searchBar.classList.add("opacity-0");
+  searchBar.classList.remove("opacity-100");
+  searchBar.classList.remove("translate-y-full");
+});
+function openSearch() {
+  searchBar.classList.add("translate-y-full");
+  searchBar.classList.add("opacity-100");
+  searchBar.classList.remove("opacity-0");
+  searchBar.classList.remove("-translate-y-full");
+}
+function closeSearch() {
+  searchBar.classList.add("-translate-y-full");
+  searchBar.classList.add("opacity-0");
+  searchBar.classList.remove("opacity-100");
+  searchBar.classList.remove("translate-y-full");
+}
