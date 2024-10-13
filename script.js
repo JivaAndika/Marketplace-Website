7f1105ddddd2ece64fccc2cf988e30187c3ad7ba
Sleeper678 <umardika678@gmail.com>
2024-10-14 06:03:02 +0700

    final rev (maybe)

--- script.js
+++ script.js
@@ -19,5 +19,54 @@ function closeSearch() {
   searchBar.classList.remove("opacity-100");
   searchBar.classList.remove("translate-y-full");
 }
+//  SIDEBAR
+const sidebar = document.querySelector("aside");
+function openNavbar() {
+  sidebar.classList.add("translate-x-0");
+  sidebar.classList.remove("-translate-x-full");
+}
+function closeNavbar() {
+  sidebar.classList.add("-translate-x-full");
+  sidebar.classList.remove("translate-x-0");
+}
+// ACTIVE COLOR PICKER
+function addActiveClass(wrapperClass) {
+  const wrapper = document.getElementsByClassName(wrapperClass)[0]; 
+  const pickers = wrapper.getElementsByClassName("button_picker");
 
-// SLIDER IMAGE
+  for (var i = 0; i < pickers.length; i++) {
+    pickers[i].addEventListener("click", function () {
+      var current = wrapper.getElementsByClassName("active");
+      if (current.length > 0) {
+        current[0].className = current[0].className.replace(" active", ""); 
+      }
+      this.className += " active"; 
+    });
+  }
+}
+
+addActiveClass("button_wrapper"); 
+addActiveClass("size_wrapper");
+
+// CLEAR FILTER
+document.getElementById("clear").addEventListener("click", function () {
+  var activeElements = document.getElementsByClassName("active");
+  while (activeElements.length > 0) {
+    activeElements[0].classList.remove("active");
+  }
+});
+
+// FILTER BAR
+// const filter_bar = document.getElementById("filter_bar");
+// filter_bar.addEventListener("click", function () {
+//   filter_bar.classList.toggle("flex");
+// });
+const filterBar = document.getElementById("filter_bar");
+function openFilter() {
+  filterBar.classList.add("translate-y-0");
+  filterBar.classList.remove("translate-y-full");
+}
+function closeFilter() {
+  filterBar.classList.add("translate-y-full");
+  filterBar.classList.remove("translate-y-0");
+}
